a4ba9b6267d571e24f216d653e65ae5d99c432e5
MadGeckoo <dk.samper@gmail.com>
2015-05-14 22:24:02 +0200

    doplneny design UI

---
+++ src/main/java/AvengersForm.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+
+/**
+ * Created by kost on 14. 5. 2015.
+ */
+public class AvengersForm {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JComboBox comboBox1;
+    private JButton updateHeroButton;
+    private JButton addNewHeroButton;
+    private JButton removeHeroButton;
+    private JTextArea getAllHeroesTextArea;
+    private JTextArea getAllVillainsTextArea;
+    private JButton removeVillainButton;
+    private JButton addNewVillainButton;
+    private JButton updateVillainButton;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JComboBox comboBox2;
+    private JTextArea getAllMissionsTextArea;
+    private JButton addNewMissionButton;
+    private JButton findMissionByHeroButton;
+    private JButton findMissionByVillainButton;
+    private JButton updateMissionButton;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JComboBox comboBox3;
+}
